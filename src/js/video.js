b0959b13d875a71a7f87633d0a7cf26b47523634
KOE <oskar@restfuldesign.com>
2015-11-28 12:27:21 +0100

    Add icons

--- src/js/video.js
+++
@@ -1,7 +0,0 @@
-//= include ['vendor/ender.js', 'vendor/blazy.js', 'includes/header.js']
-$(document).ready(function() {   
-  $("body").addClass('loaded');
-  var bLazy = new Blazy({ 
-      // options
-  });
-});
